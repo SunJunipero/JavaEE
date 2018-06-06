721a7cc9c3e0f186ce8e6af3b434dafbdb72d078
SunJunipero <tkonstantin1381@gmail.com>
2018-06-06 06:03:55 +0300

    add hibernate module, and implemented xml-mapping-annotation example

---
+++ hibernate/part1/config-xml-mapping-annotations/src/main/java/smpl/hibernate/HibernateConfigXMLMappingAnnotations.java
@@ -0,0 +1,56 @@
+package smpl.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HibernateConfigXMLMappingAnnotations {
+
+    private static final Logger log = LoggerFactory.getLogger(HibernateConfigXMLMappingAnnotations.class);
+
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    private static void init() {
+        Configuration configuration = new Configuration();
+        //configuration.addClass(smpl.hibernate.Region.class);
+        /**
+         * without this line (configuration.addPackage("smpl.hibernate").addAnnotatedClass(Region.class))
+         * we get Unknown entity exception
+         */
+        configuration.addPackage("smpl.hibernate").addAnnotatedClass(Region.class);
+        configuration.configure("hibernate.cfg.xml");
+        serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    private static void destroy() {
+        StandardServiceRegistryBuilder.destroy(serviceRegistry);
+    }
+
+    public static void main(String[] args) {
+        init();
+
+        Session s = sessionFactory.getCurrentSession();
+        s.beginTransaction();
+
+        //s.save(new hibernate.Region("Krasnoe Selo"));
+        Region o = new Region("looool");
+        s.save(o);
+
+//        List<hibernate.Region> regionList = s.createQuery("from hibernate.Region").list();
+//        for (hibernate.Region r : regionList) {
+//            System.out.println(r);
+//        }
+
+        s.getTransaction().commit();
+
+        log.debug("Transaction committed");
+
+        destroy();
+    }
+}
