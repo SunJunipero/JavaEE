35c6766f1df6c743dd1618734bc547ef2746932d
SunJunipero <tkonstantin1381@gmail.com>
2018-06-14 20:54:08 +0300

    For CRUD Books. created initial vision of jsp page (simple table), implemented simple servlet for allBooks mapping

--- SkillsExamples/phase1/CRUDEazyServletAndBd/src/main/java/com/eazycrud/example/MainServlet.java
+++ SkillsExamples/phase1/CRUDEazyServletAndBd/src/main/java/com/eazycrud/example/MainServlet.java
@@ -1,4 +1,40 @@
 package com.eazycrud.example;
 
-public class MainServlet {
+import com.eazycrud.example.DAO.BookDAO;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/books")
+public class MainServlet extends HttpServlet {
+
+    BookDAO bookDAO;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        String jdbcURL = getServletContext().getInitParameter("jdbcURL");
+        String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+        String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+
+        bookDAO = new BookDAO(jdbcURL, jdbcUsername, jdbcPassword);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            req.setAttribute("bookList", bookDAO.ListAllBook());
+        } catch (SQLException e) {
+
+        }
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+
+    }
 }
--- SkillsExamples/phase1/CRUDEazyServletAndBd/src/main/webapp/index.jsp
+++ SkillsExamples/phase1/CRUDEazyServletAndBd/src/main/webapp/index.jsp
@@ -6,11 +6,29 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Books</title>
 </head>
 <body>
-
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Price</th>
+            <th>Action</th>
+        </tr>
+        <c:forEach items="${bookList}" var="book">
+            <tr>
+                <td><c:out value="${book.id}"/></td>
+                <td><c:out value="${book.title}"/></td>
+                <td><c:out value="${book.author}"/></td>
+                <td><c:out value="${book.price}"/></td>
+                <td>action</td>
+            </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
