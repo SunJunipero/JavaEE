d281d5c720c5ed52161eed221ae24f1fd381fdd1
SunJunipero <tkonstantin1381@gmail.com>
2018-06-28 01:36:03 +0300

    created PostDAO class

---
+++ SkillsExamples/phase2/AwesomeProject-01/src/main/java/com/musicblog/dao/impl/PostDAO.java
@@ -0,0 +1,17 @@
+package com.musicblog.dao.impl;
+
+import com.musicblog.model.Post;
+
+import java.sql.ResultSet;
+
+public class PostDAO extends AbstractDAOImpl <Post> {
+    @Override
+    public String getAllQuery() {
+        return null;
+    }
+
+    @Override
+    public Post getEntity(ResultSet set) {
+        return null;
+    }
+}
